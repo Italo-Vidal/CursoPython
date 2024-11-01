58e2f5912662e3686a11c54ec6084c1e54f83628
Italo <italo.vidal1402@gmail.com>
2024-11-01 20:06:47 -0300

    ex012

--- Exercicios/ex012.py
+++ Exercicios/ex012.py
@@ -1,5 +1,5 @@
-valor = float(input('Digite o valor do produto? R$'))
+preco = float(input('Digite o valor do produto? R$'))
 
-desconto = valor - (valor * 5 / 100)
+desconto = preco - (preco * 5 / 100)
 
 print(f'O valor do produto com desconto é de {desconto:.2f}R$')
