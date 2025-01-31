71adfc1a0d11455b7203b955c143874206733d06
Italo <italo.vidal1402@gmail.com>
2025-01-31 15:24:37 -0300

    ex021_MP3

---
+++ Exercicios/ex021.py
@@ -0,0 +1,15 @@
+import pygame
+
+# Inicializa todos os módulos do pygame
+pygame.init()
+
+# Inicializa o mixer do pygame
+pygame.mixer.init()
+
+# Carrega o arquivo MP3
+pygame.mixer.music.load('ex021.mp3')
+
+# Reproduz o arquivo MP3
+pygame.mixer.music.play()
+
+parar = str(input('Aperta [ P ] para desligar: '))
